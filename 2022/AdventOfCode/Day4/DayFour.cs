fd7d198dd5b512326f83e147dea065cf93a80ba9
Andy Summerfield <aj.summerfield93@gmail.com>
2022-12-04 22:56:22 +0000

    Day 4

--- 2022/AdventOfCode/Day4/DayFour.cs
+++ 2022/AdventOfCode/Day4/DayFour.cs
@@ -1,7 +1,6 @@
 ﻿namespace AdventOfCode
 {
-    using System;
-    using System.ComponentModel;
+    using System.Collections.Generic;
     using System.Linq;
     using AdventOfCode.Helpers;
 
@@ -9,20 +8,68 @@
     {
         public static int RunPartOne()
         {
-            var input = FileReader.Read(4, true);
+            var input = FileReader.Read(4);
 
             var result = 0;
+
+            foreach (var pair in input)
+            {
+                var assignments = pair.Split(",");
+                var a1Sections = assignments[0].Split("-").Select(int.Parse);
+                var a2Sections = assignments[1].Split("-").Select(int.Parse);
+
+                if ((a1Sections.First() >= a2Sections.First() && a1Sections.Last() <= a2Sections.Last()) ||
+                    (a2Sections.First() >= a1Sections.First() && a2Sections.Last() <= a1Sections.Last()))
+                {
+                    result++;
+                }
+            }
 
             return result;
         }
 
         public static int RunPartTwo()
         {
-            var input = FileReader.Read(4, true);
+            var input = FileReader.Read(4);
 
             var result = 0;
 
+            foreach (var pair in input)
+            {
+                var assignments = pair.Split(",");
+                var a1Sections = assignments[0].Split("-").Select(int.Parse);
+                var a2Sections = assignments[1].Split("-").Select(int.Parse);
+
+                if ((a1Sections.First() >= a2Sections.First() && a1Sections.Last() <= a2Sections.Last()) ||
+                    (a2Sections.First() >= a1Sections.First() && a2Sections.Last() <= a1Sections.Last()))
+                {
+                    result++;
+                }
+                else
+                {
+                    var a1SectionNums = GetSections(a1Sections);
+                    var a2SectionNums = GetSections(a2Sections);
+
+                    if (a1SectionNums.Any(c => a2SectionNums.Contains(c)))
+                    {
+                        result++;
+                    }
+                }
+            }
+
             return result;
+        }
+
+        private static List<int> GetSections(IEnumerable<int> sections)
+        {
+            var sectionNums = new List<int>();
+
+            for (var i = sections.First(); i <= sections.Last(); i++)
+            {
+                sectionNums.Add(i);
+            };
+
+            return sectionNums;
         }
     }
 }
