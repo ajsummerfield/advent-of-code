8bc2475e6af9a9b54a9c4a1366d8b83aaa9a189f
Andy <aj.summerfield93@gmail.com>
2022-12-05 16:41:09 +0000

    Refactor Day2

--- 2022/AdventOfCode/Day2/DayTwo.cs
+++ 2022/AdventOfCode/Day2/DayTwo.cs
@@ -1,7 +1,6 @@
 ﻿namespace AdventOfCode
 {
     using System;
-    using System.ComponentModel;
     using System.Linq;
     using AdventOfCode.Helpers;
 
@@ -11,35 +10,21 @@
         {
             var input = FileReader.Read(2);
 
-            var rounds = input.Select(x => new
-            {
-                Opponent = x.Split(" ")[0],
-                You = x.Split(" ")[1]
-            }).ToList();
-
             var result = 0;
 
-            rounds.ForEach(round =>
+            input.ToList().ForEach(round =>
             {
-                if (round.Opponent == "A" && round.You == "X" ||
-                    round.Opponent == "B" && round.You == "Y" ||
-                    round.Opponent == "C" && round.You == "Z")
+                result += round switch
                 {
-                    result += 3;
-                }
-
-                if (round.You == "X" && round.Opponent == "C" ||
-                    round.You == "Y" && round.Opponent == "A" ||
-                    round.You == "Z" && round.Opponent == "B")
-                {
-                    result += 6;
-                }
-
-                result += round.You switch
-                {
-                    "X" => 1,
-                    "Y" => 2,
-                    "Z" => 3,
+                    "A X" => 3 + 1,
+                    "A Y" => 6 + 2,
+                    "A Z" => 0 + 3,
+                    "B X" => 0 + 1,
+                    "B Y" => 3 + 2,
+                    "B Z" => 6 + 3,
+                    "C X" => 6 + 1,
+                    "C Y" => 0 + 2,
+                    "C Z" => 3 + 3,
                     _ => throw new ArgumentException("Invalid Choice!")
                 };
             });
@@ -51,23 +36,23 @@
         {
             var input = FileReader.Read(2);
 
-            var rounds = input.Select(x => new
-            {
-                Opponent = x.Split(" ")[0],
-                You = x.Split(" ")[1]
-            }).ToList();
-
             var result = 0;
 
-            rounds.ForEach(round =>
+            input.ToList().ForEach(round =>
             {
                 var choice = string.Empty;
 
-                result += round.Opponent switch
+                result += round switch
                 {
-                    "A" => round.You == "X" ? 3 : round.You == "Y" ? 4 : 8,
-                    "B" => round.You == "X" ? 1 : round.You == "Y" ? 5 : 9,
-                    "C" => round.You == "X" ? 2 : round.You == "Y" ? 6 : 7,
+                    "A X" => 0 + 3,
+                    "A Y" => 3 + 1,
+                    "A Z" => 6 + 2,
+                    "B X" => 0 + 1,
+                    "B Y" => 3 + 2,
+                    "B Z" => 6 + 3,
+                    "C X" => 0 + 2,
+                    "C Y" => 3 + 3,
+                    "C Z" => 6 + 1,
                     _ => throw new ArgumentException("Invalid Choice!")
                 };
             });
