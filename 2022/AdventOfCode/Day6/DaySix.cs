5334e0730b1a2e843d99664d54a66d595a0ee087
Andy <aj.summerfield93@gmail.com>
2022-12-06 12:51:39 +0000

    Day6

---
+++ 2022/AdventOfCode/Day6/DaySix.cs
@@ -0,0 +1,26 @@
+﻿namespace AdventOfCode
+{
+    using System.Linq;
+    using AdventOfCode.Helpers;
+
+    public static class DaySix
+    {
+        public static int RunPartOne()
+        {
+            var input = FileReader.Read(6).FirstOrDefault();
+
+            var result = input.Select((x, i) => input.Skip(i).Take(4).Distinct().Count() == 4 ? i + 4 : 0).FirstOrDefault(x => x != 0);
+
+            return result;
+        }
+
+        public static int RunPartTwo()
+        {
+            var input = FileReader.Read(6).FirstOrDefault();
+
+            var result = input.Select((x, i) => input.Skip(i).Take(14).Distinct().Count() == 14 ? i + 14 : 0).FirstOrDefault(x => x != 0);
+
+            return result;
+        }
+    }
+}
