154b4ef13b1dee696c977bf3d674e25d219b91e1
Andy Summerfield <aj.summerfield93@gmail.com>
2022-12-14 16:02:38 +0000

    Finally did Day7

--- 2022/AdventOfCode/Day7/DaySeven.cs
+++ 2022/AdventOfCode/Day7/DaySeven.cs
@@ -8,53 +8,153 @@
     {
         public static int RunPartOne()
         {
-            var input = FileReader.Read(7, true).ToList();
+            var input = FileReader.Read(7).ToList();
 
             var system = new Dictionary<string, List<string>>();
 
-            var directories = new List<string>();
+            var directories = new Dictionary<string, int>();
 
             var currentDir = string.Empty;
+
+            var path = "/";
 
             input.ForEach(x =>
             {
                 if (x.StartsWith("$ cd"))
                 {
-                    var cdRequest = x.Split(" ").Last();
-
-                    if (cdRequest == "..")
+                    if (x.Split(" ").Last() == "..")
                     {
-                        var currentDirIndex = directories.IndexOf(currentDir);
-                        currentDir = directories[currentDirIndex - 1];
+                        path = path.Remove(path.IndexOf(currentDir + "/"), currentDir.Length + 1);
+                        currentDir = path != "/" ? path.Split("/").Last(x => x != string.Empty) : "/";
                         return;
                     }
 
                     currentDir = x.Split(" ").Last();
 
-                    if (!system.ContainsKey(currentDir))
+                    if (currentDir != "/")
                     {
-                        system.Add(currentDir, new List<string>());
-                        directories.Add(currentDir);
+                        path = path + currentDir + "/";
+                    }
+
+                    if (!system.ContainsKey(path))
+                    {
+                        system.Add(path, new List<string>());
+                        directories.Add(path, 0);
                     }
                 }
 
                 if (!x.StartsWith("$"))
                 {
-                    system.GetValueOrDefault(currentDir).Add(x);
+                    system.GetValueOrDefault(path).Add(x);
+
+                    if (!x.Contains("dir"))
+                    {
+                        var fileSize = int.Parse(x.Split(" ").First());
+                        var currentDirSize = directories.GetValueOrDefault(path);
+                        directories.Remove(path);
+                        directories.Add(path, currentDirSize + fileSize);
+                    }
                 }
             });
 
-            var result = 0;
+            foreach (var dir in system.Reverse())
+            {
+                var innerDirs = dir.Value.FindAll(x => x.Contains("dir")).Select(x => $"{dir.Key}{x.Split(" ").Last()}/");
+
+                if (innerDirs.Any())
+                {
+                    var innerDirsSize = directories.Where(x => innerDirs.Contains(x.Key)).Sum(x => x.Value);
+
+                    if (innerDirsSize > 0)
+                    {
+                        var currentDirSize = directories.GetValueOrDefault(dir.Key);
+
+                        directories.Remove(dir.Key);
+                        directories.Add(dir.Key, currentDirSize + innerDirsSize);
+                    }
+                }
+            }
+
+            var result = directories.Where(x => x.Value < 100000).Sum(x => x.Value);
 
             return result;
         }
 
         public static int RunPartTwo()
         {
-            var input = FileReader.Read(7, true).ToList();
+            var input = FileReader.Read(7).ToList();
 
-            var result = 0;
+            var system = new Dictionary<string, List<string>>();
 
+            var directories = new Dictionary<string, int>();
+
+            var currentDir = string.Empty;
+
+            var path = "/";
+
+            input.ForEach(x =>
+            {
+                if (x.StartsWith("$ cd"))
+                {
+                    if (x.Split(" ").Last() == "..")
+                    {
+                        path = path.Remove(path.IndexOf(currentDir + "/"), currentDir.Length + 1);
+                        currentDir = path != "/" ? path.Split("/").Last(x => x != string.Empty) : "/";
+                        return;
+                    }
+
+                    currentDir = x.Split(" ").Last();
+
+                    if (currentDir != "/")
+                    {
+                        path = path + currentDir + "/";
+                    }
+
+                    if (!system.ContainsKey(path))
+                    {
+                        system.Add(path, new List<string>());
+                        directories.Add(path, 0);
+                    }
+                }
+
+                if (!x.StartsWith("$"))
+                {
+                    system.GetValueOrDefault(path).Add(x);
+
+                    if (!x.Contains("dir"))
+                    {
+                        var fileSize = int.Parse(x.Split(" ").First());
+                        var currentDirSize = directories.GetValueOrDefault(path);
+                        directories.Remove(path);
+                        directories.Add(path, currentDirSize + fileSize);
+                    }
+                }
+            });
+
+            foreach (var dir in system.Reverse())
+            {
+                var innerDirs = dir.Value.FindAll(x => x.Contains("dir")).Select(x => $"{dir.Key}{x.Split(" ").Last()}/");
+
+                if (innerDirs.Any())
+                {
+                    var innerDirsSize = directories.Where(x => innerDirs.Contains(x.Key)).Sum(x => x.Value);
+
+                    if (innerDirsSize > 0)
+                    {
+                        var currentDirSize = directories.GetValueOrDefault(dir.Key);
+
+                        directories.Remove(dir.Key);
+                        directories.Add(dir.Key, currentDirSize + innerDirsSize);
+                    }
+                }
+            }
+
+            var remainingSpace = 70000000 - directories.FirstOrDefault().Value;
+
+            var requiredExtraSpace = 30000000 - remainingSpace;
+
+            var result = directories.OrderByDescending(x => x.Value).Reverse().FirstOrDefault(x => x.Value > requiredExtraSpace).Value;
+            
             return result;
         }
     }
